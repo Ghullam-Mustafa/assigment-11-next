b2869f3fb7ef04e49c109e30f3e2fa1df6adeb23
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-08-23 01:23:46 +0500

    project cmplete

---
+++ app/(components)/testimonialCard/testimonialCard.js
@@ -0,0 +1,20 @@
+
+
+export default function testimonialCard(props) {
+  return (
+    <div class="testimonials-col">
+    <img src={props.src} alt="oo.."/>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi maiores in nostrum rerum voluptatem
+            praesentium veritatis alias omnis voluptate nisi aperiam, voluptatum quibusdam itaque a
+            deserunt. In quia repellat maxime.</p>
+        <h3>Student Name</h3>
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+    </div>
+</div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,99 +1,93 @@
 import Navbar from "./(components)/navbar/navbar"
 import FasilitiesCard from "./(components)/facilitiesCard/facilitiesCard"
 import Imeges from "./(components)/imegesCard/imegesCard"
+import TestimonialCard from "./(components)/testimonialCard/testimonialCard"
 import Card from "./(components)/card/card"
 
-function Home(){
-return(
-  <>
-   <section className="header">
-        <Navbar/>
+function Home() {
+    return (
+        <>
+            <section className="header">
+                <Navbar />
 
-        <div className="text_box">
-            <h2>GET READY</h2>
-            <p id="headtext">TO DISCOVER CAMPUS</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit consequuntur corrupti sapiente aut porro
-                <br/> esse blanditiis in quae perspiciatis quo.
-            </p>
-            <a href="#" className="hero_btn">Visit Us To Know More</a>
-        </div>
-    </section>
-  
-
-    <section className="course">
-        <div className="PageBlock">
-            <div className="verticalLine"></div>
-            <div className="Clear"></div>
-        </div>
-        <h1>EXPLORE OUR 60+ <br/> MAJOR PROGRAMS</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-
-        <div className="row">
-           <Card heading="Undergraduate Programs" />
-           <Card heading="Graduate Programs" />
-           <Card heading="Adult Learning & Degree Completion" />
-        </div>
-    </section>
-
-
-    <section className="campus">
-        <div className="PageBlock">
-            <div className="verticalLine"></div>
-            <div className="Clear"></div>
-        </div>
-        <h1>TAKE OUR VIRTUAL TOUR</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-
-        <div className="row">
-          
-            <Imeges heading="DELHI" src="img/Campus1.png" >  </Imeges>
-            <Imeges heading="HYDERABAD" src="img/Campus2.png" >  </Imeges>
-            <Imeges heading="MUMBAI" src="img/Campus3.png" >  </Imeges>
-        </div>
-    </section>
-
-    <section class="facilities">
-        <div class="PageBlock">
-            <div class="verticalLine"></div>
-            <div class="Clear"></div>
-        </div>
-        <h1>Our Facilities</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-
-        <div class="row">
-           <FasilitiesCard src="img/Libary.png" heading="Best Libary" />
-           <FasilitiesCard src="img/playground.png" heading="Athletics" />
-           <FasilitiesCard src="img/food.png" heading="Tasty and Healthy Food" />
-        </div>
-    </section>
-
-    <section class="testimonials">
-        <div class="PageBlock">
-            <div class="verticalLine"></div>
-            <div class="Clear"></div>
-        </div>
-        <h1>What Our Student Says</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-
-        <div class="row">
-         
-            <div class="testimonials-col">
-                <img src="img/user.png" alt="oo.."/>
-                <div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi maiores in nostrum rerum voluptatem
-                        praesentium veritatis alias omnis voluptate nisi aperiam, voluptatum quibusdam itaque a
-                        deserunt. In quia repellat maxime.</p>
-                    <h3>Student Name</h3>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star-half-alt"></i>
+                <div className="text_box">
+                    <h2>GET READY</h2>
+                    <p id="headtext">TO DISCOVER CAMPUS</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit consequuntur corrupti sapiente aut porro
+                        <br /> esse blanditiis in quae perspiciatis quo.
+                    </p>
+                    <a href="#" className="hero_btn">Visit Us To Know More</a>
                 </div>
-            </div>
-        </div>
-    </section>
-  </>
-)
+            </section>
+
+
+            <section className="course">
+                <div className="PageBlock">
+                    <div className="verticalLine"></div>
+                    <div className="Clear"></div>
+                </div>
+                <h1>EXPLORE OUR 60+ <br /> MAJOR PROGRAMS</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+
+                <div className="row">
+                    <Card heading="Undergraduate Programs" />
+                    <Card heading="Graduate Programs" />
+                    <Card heading="Adult Learning & Degree Completion" />
+                </div>
+            </section>
+
+
+            <section className="campus">
+                <div className="PageBlock">
+                    <div className="verticalLine"></div>
+                    <div className="Clear"></div>
+                </div>
+                <h1>TAKE OUR VIRTUAL TOUR</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+
+                <div className="row">
+
+                    <Imeges heading="DELHI" src="img/Campus1.png" >  </Imeges>
+                    <Imeges heading="HYDERABAD" src="img/Campus2.png" >  </Imeges>
+                    <Imeges heading="MUMBAI" src="img/Campus3.png" >  </Imeges>
+                </div>
+            </section>
+
+            <section class="facilities">
+                <div class="PageBlock">
+                    <div class="verticalLine"></div>
+                    <div class="Clear"></div>
+                </div>
+                <h1>Our Facilities</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+
+                <div class="row">
+                    <FasilitiesCard src="img/Libary.png" heading="Best Libary" />
+                    <FasilitiesCard src="img/playground.png" heading="Athletics" />
+                    <FasilitiesCard src="img/food.png" heading="Tasty and Healthy Food" />
+                </div>
+            </section>
+
+            <section class="testimonials">
+                <div class="PageBlock">
+                    <div class="verticalLine"></div>
+                    <div class="Clear"></div>
+                </div>
+                <h1>What Our Student Says</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+
+                <div class="row">
+                    <TestimonialCard src="img/user.png" />
+                    <TestimonialCard src="img/user.png" />
+
+                </div>
+
+                <section class="cta">
+                    <h1>GET READY FOR A BRIGHT FUTURE</h1>
+                    <a href="contact.html" class="hero_btn">CONTACT US</a>
+                </section>
+            </section>
+        </>
+    )
 }
 export default Home
